08a3ffa68ea9c49c5df5a4c865ca7fc91eee8d8e
mengxu.li <mengxu.li@yingzt.com>
2020-04-21 16:52:42 +0800

    add 64

--- 64.最小路径和.js
+++ 64.最小路径和.js
@@ -8,6 +8,22 @@
  * @return {number}
  */
 var minPathSum = function(grid) {
-    let 
+
+    let y = grid.length - 1
+    let x = grid[0].length - 1
+
+    for (let i = y; i >= 0; i--) {
+        for (let j = x; j >= 0; j--) {
+            if (i !== y && j === x) {
+                grid[i][j] = grid[i][j] + grid[i+1][j]
+            } else if (i === y && j !== x) {
+                grid[i][j] = grid[i][j] + grid[i][j + 1]
+            } else if (i !== y && j !== x) {
+                grid[i][j] = grid[i][j] + Math.min(grid[i][j+1], grid[i+1][j])
+            }
+        }
+    }
+
+    return grid[0][0]
 };
 
